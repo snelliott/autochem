2fe8800d8dd5abf94121e9cc3cd65b4c407d68d4
Andreas Copan <avcopan@gmail.com>
2025-04-23 21:40:55 -0400

    New: Tests autochem.therm submodule

---
+++ autochem/tests/test__therm.py
@@ -0,0 +1,43 @@
+"""Test autochem.rate."""
+
+import pytest
+
+from autochem import therm
+
+NASA7 = """
+! NASA polynomial coefficients for CH2O3
+CH2O3(82)               H   2C   1O   3     G   100.000  5000.000  956.80      1
+    1.18953474E+01 1.71602859E-03-1.47932622E-07 9.25919346E-11-1.38613705E-14    2
+-7.81027323E+04-3.82545468E+01 2.98116506E+00 1.14388942E-02 2.77945768E-05    3
+-4.94698411E-08 2.07998858E-11-7.51362668E+04 1.09457245E+01                   4
+"""
+
+
+@pytest.mark.parametrize(
+    "name, spc_str0",
+    [
+        ("NASA7", NASA7),
+    ],
+)
+def test__from_chemkin_string(name, spc_str0):
+    spc = therm.from_chemkin_string(spc_str0)
+    spc_ = therm.Species.model_validate(spc.model_dump())
+    assert spc == spc_, f"\n   {spc}\n!= {spc_}"
+
+    spc_str = therm.chemkin_string(spc)
+    print(spc_str)
+    spc_ = therm.from_chemkin_string(spc_str)
+    assert spc == spc_, f"\n   {spc}\n!= {spc_}"
+
+    # Evaluate
+    print("Evaluation not yet tested")
+
+    # Scale
+    print("Scaling not yet implemented")
+
+    # Plot
+    print("Plotting not yet implemented")
+
+
+if __name__ == "__main__":
+    test__from_chemkin_string("NASA7", NASA7)
