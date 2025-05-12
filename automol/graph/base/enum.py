dd2b019dbae5c2b914aa4426f5507933cce96a9d
Andreas Copan <avcopan@gmail.com>
2025-05-12 16:53:02 -0400

    New: QOOH instability SMARTS template

--- automol/graph/base/enum.py
+++ automol/graph/base/enum.py
@@ -54,6 +54,7 @@ class ReactionSmarts:
         f"[{Ar}:1]-[{A_}:2][O:3][O:4][H:5]>>([{Ar}:1]=[{A_}:2]).([O:3][O:4][H:5])"
     )
     qooh_ring_forming_scission = f"([{Cr}:1].[{O_}:2][O:3][H:4])>>[R:1][R:2].[O:3][H:4]"
+    qooh_instability = f"[{Cr}:1][O:2][O:3][H:4]>>[C:1]=[O:2].[O:3][H:4]"
 
 
 def reactions(smarts: str, gra: object, symeq: bool = False) -> list[object]:
